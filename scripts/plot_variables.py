740d9a94a7e4d3a85074beeabc841de6a8b0390b
Onkur Sen <onkursen@gmail.com>
2013-03-08 13:45:37 -0600

    updates

--- scripts/plot_variables.py
+++ scripts/plot_variables.py
@@ -1,26 +1,43 @@
-from ROOT import TH1F, TCanvas, TLegend
-import os
+import ROOT, os
 
-def plot_var(var, directory):
-  histo = TH1F(
+def plot_var(var):
+  c1 = ROOT.TCanvas("c1","c1",800,800)
+
+  susy = ROOT.TH1F(
     var.upper(),
-    "Distribution of %s for Top Quark %s" % 
-    (var[:-1].upper(), var[-1].upper()),
+    "Distribution of %s: SUSY (Blue) vs. ttj (Red)" % var.upper(),
     45, 0, 900)
-  for line in open("output/output_%s/%s.txt" % (directory, var)):
-    histo.Fill(float(line))
+  for line in open("output/susy/%s.txt" % var): susy.Fill(float(line))
+  susy.SetLineColor(ROOT.kBlue)
+  susy.SetLineWidth(2)
+  # susy.Draw()
 
-  c1 = TCanvas("c1","c1",800,800)
-  histo.Draw()
-  c1.SaveAs("plots/plot_%s_%s.png" % (directory, var))
+  ttj = ROOT.TH1F(
+    var.upper(),
+    "Distribution of %s: SUSY (Blue) vs. ttj (Red)" % var.upper(),
+    45, 0, 900)
+  for line in open("output/ttj/%s.txt" % var): ttj.Fill(float(line))
+  ttj.SetLineColor(ROOT.kRed)
+  ttj.SetLineWidth(2)
+  # ttj.Draw("same")
+
+  hs = ROOT.THStack("hs","Distribution of %s: SUSY (Blue) vs. ttj (Red)" % var.upper())
+  hs.Add(susy)
+  hs.Add(ttj)
+  hs.Draw()
+
+  c1.SaveAs("plots/plot_%s.png" % var)
 
 def plot_angle(var):
-  c1 = TCanvas("c1","c1",800,800)
+  c1 = ROOT.TCanvas("c1","c1",800,800)
 
-  susy = TH1F("Phi_SUSY", "Azimuthal Angles", 35, 0, 3.5)
-  for line in open("output/output_susy/angles_%s.txt" % var):
+  susy = ROOT.TH1F("%s_angles" % var,
+    "Azimuthal Angles: SUSY (Blue) vs. ttj (Red)",
+    35, 0, 3.5)
+  for line in open("output/susy/angles_%s.txt" % var):
     susy.Fill(float(line))
-  susy.SetLineColor(4) # blue
+  susy.SetLineColor(ROOT.kBlue)
+  susy.SetLineWidth(2)
   if susy.Integral()!=0: susy.Scale(1/susy.Integral())
   susy.SetMaximum(0.06)
   susy.GetYaxis().SetTitleSize(0.05)
@@ -30,37 +47,26 @@ def plot_angle(var):
   susy.SetYTitle("Fraction / 0.1")
   susy.Draw()
 
-  ttj = TH1F("Phi_ttj", "Azimuthal Angles of Bottom Quark", 35, 0, 3.5)
-  for line in open("output/output_ttj/angles_%s.txt" % var):
+  ttj = ROOT.TH1F("%s_angles" % var,
+    "Azimuthal Angles of Bottom Quark", 35, 0, 3.5)
+  for line in open("output/ttj/angles_%s.txt" % var):
     ttj.Fill(float(line))
-  ttj.SetLineColor(8) # green
+  ttj.SetLineColor(ROOT.kRed)
+  ttj.SetLineWidth(2)
   if (ttj.Integral()!=0): ttj.Scale(1/ttj.Integral())
   ttj.Draw("same")
-
-  leg_hist = TLegend(0.7,0.8,0.9,0.95)
-  leg_hist.AddEntry(susy,"SUSY","l")
-  leg_hist.AddEntry(ttj,"ttj","l")
-  leg_hist.Draw()
 
   c1.SaveAs("plots/plot_angles_%s.png" % var)
 
 if not os.path.isdir('plots'): os.system('mkdir plots')
 
-plot_var('m3a', 'susy')
-plot_var('m3a', 'ttj')
-print 'plotted m3a'
-plot_var('m2a', 'susy')
-plot_var('m2a', 'ttj')
-print 'plotted m2a'
-plot_var('m3b', 'susy')
-plot_var('m3b', 'ttj')
-print 'plotted m3b'
-plot_var('m2b', 'susy')
-plot_var('m2b', 'ttj')
-print 'plotted m2b'
-plot_angle('b')
-print 'plotted b angle'
-plot_angle('j1')
-print 'plotted j1 angle'
-plot_angle('j2')
-print 'plotted j2 angle'
+variables = ['m3a', 'm2a', 'm3b', 'm2b']
+angles = ['b', 'j1', 'j2']
+
+for v in variables:
+  plot_var(v)
+  print 'Plotted', v
+
+for a in angles:
+  plot_angle(a)
+  print 'Plotted', a, 'angle'
